300f0dd6345e867141d3983b4af8341b0b294a7a
tuxudo <tuxudo@users.noreply.github.com>
2023-03-03 11:11:17 -0500

    Update pci_model.php

--- pci_model.php
+++ pci_model.php
@@ -23,44 +23,44 @@ class Pci_model extends \Model {
 		$this->rs['subsystem_vendor_id'] = '';
 		$this->rs['vendor_id'] = '';
 
-        if ($serial) {
-            $this->retrieve_record($serial);
-        }
+		if ($serial) {
+			$this->retrieve_record($serial);
+		}
 
 		$this->serial_number = $serial;
 	}
 	
-	// ------------------------------------------------------------------------
-   
-     /**
-     * Get PCI device names for widget
-     *
-     **/
-     public function get_pci_devices()
-     {
-        $out = array();
-        $sql = "SELECT COUNT(CASE WHEN name <> '' AND name IS NOT NULL THEN 1 END) AS count, name 
-                FROM pci
-                LEFT JOIN reportdata USING (serial_number)
-                ".get_machine_group_filter()."
-                GROUP BY name
-                ORDER BY count DESC";
-        
-        foreach ($this->query($sql) as $obj) {
-            if ("$obj->count" !== "0") {
-                $obj->name = $obj->name ? $obj->name : 'Unknown';
-                $out[] = $obj;
-            }
-        }
-        return $out;
-     }
+// ------------------------------------------------------------------------
 
 	/**
-	 * Process data sent by postflight
-	 *
-	 * @param string data
-	 * @author tuxudo
-	 **/
+	* Get PCI device names for widget
+	*
+	**/
+	public function get_pci_devices()
+	{
+		$out = array();
+		$sql = "SELECT COUNT(CASE WHEN name <> '' AND name IS NOT NULL THEN 1 END) AS count, name 
+			FROM pci
+			LEFT JOIN reportdata USING (serial_number)
+			".get_machine_group_filter()."
+			GROUP BY name
+			ORDER BY count DESC";
+
+		foreach ($this->query($sql) as $obj) {
+			if ("$obj->count" !== "0") {
+				$obj->name = $obj->name ? $obj->name : 'Unknown';
+				$out[] = $obj;
+			}
+		}
+		return $out;
+	}
+
+	/**
+	* Process data sent by postflight
+	*
+	* @param string data
+	* @author tuxudo
+	**/
 	function process($plist)
 	{
 		// Check if we have data
@@ -68,16 +68,17 @@ class Pci_model extends \Model {
 			throw new Exception("Error Processing Request: No property list found", 1);
 		}
 
-		// Delete previous set        
+		// Delete previous set
 		$this->deleteWhere('serial_number=?', $this->serial_number);
 
 		$parser = new CFPropertyList();
 		$parser->parse($plist, CFPropertyList::FORMAT_XML);
 		$myList = $parser->toArray();
 
-        foreach ($this->rs as $device) {
+		foreach ($myList as $device) {
+
 			// Check if we have a name
-			if( ! array_key_exists("name", $device)){
+			if( ! is_array($device) || ! array_key_exists("name", $device)){
 				continue;
 			}
 
@@ -86,7 +87,7 @@ class Pci_model extends \Model {
 				if(array_key_exists($key, $device))
 				{
 					$this->rs[$key] = $device[$key];
-				} else {
+				} else if ($key != "serial_number") {
 					$this->rs[$key] = null;
 				}
 			}
